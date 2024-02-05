cc2741b33a5b994526ed7d9f154ed0741727f3ab
neoskurt <49443524+neoskurt@users.noreply.github.com>
2024-02-05 17:44:02 +0100

    cavalier

--- UnitTest/chess.test.js
+++ UnitTest/chess.test.js
@@ -31,14 +31,15 @@ test('Déplacement valide d\'un pion vers 2 case en haut', () => {
 });
 
 
+
 test('Déplacement valide d\'un cavalier', () => {
 
-  const initialPosition = { row: 3, col: 3 }; 
-  const validFinalPosition1 = { row: 4, col: 2 }; 
-  const validFinalPosition2 = { row: 2, col: 4 };
+  const initialPosition = { row: 4, col: 4 }; 
+  const validFinalPosition1 = { row: 2, col: 5}; 
+  const validFinalPosition2 = { row: 6, col: 3 };
 
-  const invalidFinalPosition1 = { row: 2, col: 2 }; 
-  const invalidFinalPosition2 = { row: 4, col: 4 }; 
+  const invalidFinalPosition1 = { row: 5, col: 3 }; 
+  const invalidFinalPosition2 = { row: 4, col: 2 }; 
 
   expect(isValidKnightMove(initialPosition, validFinalPosition1)).toBe(true);
   expect(isValidKnightMove(initialPosition, validFinalPosition2)).toBe(true);
--- UnitTest/knight.js
+++ UnitTest/knight.js
@@ -1,7 +1,8 @@
 function isValidKnightMove(initialPosition, finalPosition) {
+    console.log( finalPosition.row , initialPosition.row)
+
     const rowDiff = Math.abs(finalPosition.row - initialPosition.row);
     const colDiff = Math.abs(finalPosition.col - initialPosition.col);
-  
     return (rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2);
   }
   
