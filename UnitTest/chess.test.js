d774e1e99d9aa3399ea03614844f19c6e8288e03
Valouuu94 <95298655+Valouuu94@users.noreply.github.com>
2024-02-05 17:09:22 +0100

    oe

--- UnitTest/chess.test.js
+++ UnitTest/chess.test.js
@@ -1,33 +1,30 @@
-const { createEmptyChessBoard } = require('./chess');
+// chess.test.js
+const { createEmptyChessBoard, movePiece } = require('./chess');
+const { isValidPawnMove } = require('./pawn');
 
-test('Création d\'un plateau d\'échecs vide', () => {
-  const board = createEmptyChessBoard();
-  expect(board).toHaveLength(8); // Vérifie la taille du plateau
-  expect(board[0]).toHaveLength(8); // Vérifie la taille de la première rangée
-  expect(board[0][0]).toBeNull(); // Vérifie que la première case est vide
+test('Déplacement valide d\'un pion vers 1 case en haut', () => {
+    const board = createEmptyChessBoard();
+    const pawn = { type: 'pawn', color: 'white' };
+    // Position initiale du pion
+    const initialPosition = { row: 6, col: 0 }; // Bas du tableau, ligne 2
+    const validFinalPosition = { row: 5, col: 0 }; // Simuler un déplacement d'une case vers le haut
+    board[initialPosition.row][initialPosition.col] = pawn;
+    movePiece(board, initialPosition, validFinalPosition);
+  // Tester la validité du mouvement du pion avec la fonction spécifique isValidPawnMove
+    const validPawnMove = isValidPawnMove(board, pawn.color, initialPosition, validFinalPosition);
+    console.log('isValidPawnMove :', validPawnMove);
+    expect(validPawnMove).toBe(true);
 });
-const { movePiece } = require('./chess');
 
-test('Déplacement d\'une pièce sur le plateau', () => {
-  const board = createEmptyChessBoard();
-  const initialPosition = { row: 0, col: 0 };
-  const finalPosition = { row: 2, col: 2 };
-  const pieceToMove = board[initialPosition.row][initialPosition.col];
-  
-  movePiece(board, initialPosition, finalPosition);
-  
-  expect(board[initialPosition.row][initialPosition.col]).toBeNull(); // Vérifie que la case initiale est vide
-  expect(board[finalPosition.row][finalPosition.col]).toBe(pieceToMove); // Vérifie que la pièce est déplacée à la nouvelle position
-});
-const { isValidMove } = require('./chess');
-
-test('Validation d\'un mouvement de pièce', () => {
-  const board = createEmptyChessBoard();
-  const initialPosition = { row: 0, col: 0 };
-  const finalPosition = { row: 2, col: 2 };
-
-  // Assumez qu'une pièce peut se déplacer en diagonale
-  const isValid = isValidMove(board, initialPosition, finalPosition);
-  
-  expect(isValid).toBe(true);
+test('Déplacement valide d\'un pion vers 2 case en haut', () => {
+    const board = createEmptyChessBoard();
+    const pawn = { type: 'pawn', color: 'white' };
+    const initialPosition = { row: 6, col: 0 }; // Bas du tableau, ligne 2
+    const validFinalPosition = { row: 4, col: 0 }; // Simuler un déplacement d'une case vers le haut
+    board[initialPosition.row][initialPosition.col] = pawn;
+    movePiece(board, initialPosition, validFinalPosition);
+  // Tester la validité du mouvement du pion avec la fonction spécifique isValidPawnMove
+    const validPawnMove = isValidPawnMove(board, pawn.color, initialPosition, validFinalPosition);
+    console.log('isValidPawnMove :', validPawnMove);
+    expect(validPawnMove).toBe(true);
 });
